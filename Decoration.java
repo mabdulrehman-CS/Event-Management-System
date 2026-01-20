fd9ed534fbf13777c170b072324aae07e11dd887
mabdulrehman-CS <mabdulrehman-CS@users.noreply.github.com>
2026-01-20 19:10:46 +0500

    Update Decoration.java

--- Decoration.java
+++ Decoration.java
@@ -4,4 +4,23 @@ public class Decoration implements Serializable {
     private String Dcoration_Items;
     private int Decoration_Items_Quantity;
 
+    public Decoration() {
 
+    }
+
+    public String getDcoration_Items() {
+        return Dcoration_Items;
+    }
+
+    public void setDcoration_Items(String dcoration_Items) {
+        Dcoration_Items = dcoration_Items;
+    }
+
+    public int getDecoration_Items_Quantity() {
+        return Decoration_Items_Quantity;
+    }
+
+    public void setDecoration_Items_Quantity(int decoration_Items_Quantity) {
+        Decoration_Items_Quantity = decoration_Items_Quantity;
+    }
+}
