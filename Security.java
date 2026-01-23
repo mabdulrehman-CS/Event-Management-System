4a87a626aa760d9e33032724abe77b4da6db1240
mabdulrehman-CS <mabdulrehman-CS@users.noreply.github.com>
2026-01-23 08:45:46 +0500

    Update Security.java

--- Security.java
+++ Security.java
@@ -62,4 +62,69 @@ public class Security implements Serializable {
     public void setSecurity_Key(double security_Key) {
         Security_Key = security_Key;
     }
+    
+    public void Sign_up() {
+        System.out.println("Enter your details for Sign_up");
+        System.out.println("Enter Your Name");
+        this.User_Name = sc.nextLine();
+        System.out.println("Enter Your PhoneNumber");
+        this.PhoneNumber = sc.nextLine();
+        System.out.println("Enter Your CNIC");
+        this.CNIC = sc.nextLine();
+        System.out.println("Enter Your Gmail");
+        this.Gmail = sc.nextLine();
+        System.out.println("Enter Passward");
+        this.Passward = sc.nextLine();
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter("SecurityPassward.txt"));
+            bw.write(this.User_Name);
+            bw.write(",");
+            bw.write(this.Passward);
+            bw.write(",");
+            bw.write(this.PhoneNumber);
+            bw.write(",");
+            bw.write(this.CNIC);
+            bw.write(",");
+            bw.write(this.Gmail);
+            bw.newLine();
+        } catch (Exception ee) {
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (Exception ee) {
+            }
+        }
+    }
+
+    public boolean Login() {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("SecurityPassward.txt"));
+            String st;
+            while ((st = br.readLine()) != null) {
+                String[] sta = st.split(",");
+                this.User_Name = sta[0].trim();
+                this.Passward = sta[1].trim();
+            }
+        } catch (IOException io) {
+        } catch (Exception ex) {
+        }
+
+        System.out.println("Enter Username and Passward for Login");
+        System.out.println("Enter User Name");
+        String us = sc.nextLine();
+        System.out.println("Enter Passward");
+        String pa = sc.nextLine();
+        if (us.equals(getUserName()) & pa.equals(getPassward())) {
+            System.out.println("Login Successfull");
+            return true;
+        } else {
+            System.out.println("Wrong UserName or Passward");
+            return false;
+        }
+    }
+}
 
