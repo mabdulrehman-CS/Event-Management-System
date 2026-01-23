1f9ee3114a1b0827c64baef11b3ad97dc83e1e89
mabdulrehman-CS <mabdulrehman-CS@users.noreply.github.com>
2026-01-23 08:41:40 +0500

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,3 +1,17 @@
 import java.io.*;
 import java.util.*;
+public class Example implements Serializable {
+    private static Scanner sc = new Scanner(System.in);
+    public Example() {
+        
+    }
+
+    public void setSc(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public Scanner getSc() {
+        return sc;
+    }
+}
 
